14d43ffe8dfe5b01689151135dd1c763d67c69fb
Kimngan1303 <kimngan1303@gmail.com>
2026-07-27 02:07:20 +0700

    Hardcode DB string

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/auramusic');
+    const conn = await mongoose.connect('mongodb+srv://kngan13032004_db_user:Kimngan%400@cluster0.lped0ju.mongodb.net/auramusic?appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
