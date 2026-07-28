4238f1685181c03ff5f7bd22d4cb2ba2a5fddc9a
Kimngan1303 <kimngan1303@gmail.com>
2026-07-28 21:01:41 +0700

    fix(backend): use environment variable for MONGODB_URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb+srv://kngan13032004_db_user:Kimngan%400@cluster0.lped0ju.mongodb.net/auramusic?appName=Cluster0';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://kngan13032004_db_user:Kimngan%400@ac-1l1ffjp-shard-00-00.lped0ju.mongodb.net:27017,ac-1l1ffjp-shard-00-01.lped0ju.mongodb.net:27017,ac-1l1ffjp-shard-00-02.lped0ju.mongodb.net:27017/auramusic?ssl=true&replicaSet=atlas-9nvkhg-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) {
