c22f20cfc0e45ae89c109d5eb4b2cfcb0b1b7d9b
Kimngan1303 <kimngan1303@gmail.com>
2026-08-06 15:24:25 +0700

    fix: force IPv4 for MongoDB to fix Render timeout

--- server/config/db.js
+++ server/config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
+      family: 4
     });
     console.log('MongoDB Connected');
   } catch (error) {
